94c2a660db2c2434065aad3cd889fc239e6c6998
Sanket_Nimsatkar <121743873+nimsatkarS@users.noreply.github.com>
2023-05-04 06:13:31 +0530

    Array and Objact in java Script

---
+++ arry-in-js.js
@@ -0,0 +1,29 @@
+
+ //********Array of objects**************     
+ let myvar = 23;
+ let myvar2="string";
+ let myvar3=true;
+ let myvar4=null;
+ //let myvar5=undifined;
+
+ console.log(myvar);
+
+   let employee ={
+  name:"sanket",
+  chainel:"back to source",
+    salary:20,
+   
+   }
+   console.log(employee) 
+
+  //let names =[1,2,3,"sanket"];
+  let names = new Array(1,2,3, "Sami" ,"defined");
+   console.log(names.length);
+   console.log(names);
+   
+   names=names.sort;
+   names.push("yhis isvthe array")
+   console.log(names);
+   console.log(names[2]);
+
+
