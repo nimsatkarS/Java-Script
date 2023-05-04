0e9fced64a0f3505ee8bbe526465064921f465f4
Sanket_Nimsatkar <121743873+nimsatkarS@users.noreply.github.com>
2023-05-04 06:14:31 +0530

    Scope, if-else in js

---
+++ scope&if-else.js
@@ -0,0 +1,54 @@
+var string1 = "This is a another string";
+var string2 = "This is a 2 another string1"; 
+console.log(string1)
+
+//let a="u";
+{
+  let a="1ANKITA"
+}
+console.log(a)
+
+//const a ="this is the constant string"
+// a = "This is a another string in to the js"
+//console.log(a)
+
+const a ="this is the constant string"
+// a = "This is a another string in to the js"
+console.log(a)
+
+//The if-else condition will start her
+let age=20;
+if (age>19)
+ {
+console.log("Your are eligible");
+}
+else if(age==5){
+console.log("Age is 5");
+}
+else if(age==10){
+  console.log("Age is 10"); 
+}
+else{
+  console.log("you can drink coll drink");
+}
+
+
+const glas =4;
+switch(glas){
+
+  case 45:
+console.log("this is the first time 45")
+break;
+
+case 4:
+console.log("this is the first time 4")
+break;
+
+case 46:
+console.log("this is the first time 46")
+break;
+
+default:
+console.log("non of this");
+break;
+}
