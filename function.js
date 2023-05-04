4fc132d0b71ee615fa432e4eac119343eb2154c9
Sanket_Nimsatkar <121743873+nimsatkarS@users.noreply.github.com>
2023-05-04 06:17:07 +0530

    Function in js

---
+++ function.js
@@ -0,0 +1,48 @@
+//Function in javascript
+
+console.log("this is the tute1 ");
+
+function seet(name,seetext){
+
+    console.log(name +"thr codders boys");
+    console.log(name + " "+ seetext);
+  }
+  
+  function sum(a,b,c){
+    let d = a + b + c;
+    return d;
+  }
+  
+  let name = "sanket";    
+  let name1 ="mayur";
+  let name2 ="rohit";
+  let name3 ="dheraj";
+  let seetext = "good morning sanket";
+  seet(name,seetext);
+  seet(name1,seetext);
+  seet(name2,seetext);
+  seet(name3,seetext);
+  seet(seetext,seetext);
+  
+  let returnVal =sum(1,2,3);
+  console.log(returnVal)
+  
+  function sum(a,b){
+    let c= a + b;
+    return c;
+  }
+    
+  let question ="It is practice of the js";
+console.log(question);
+
+let san ="sanket"
+console.log(san);
+
+let str = "THIS IS A STRING";
+console.log(str)
+
+let position = str.lastindexOf('IS')
+console.log(position)
+  
+  
+         
