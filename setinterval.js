44fc2cd142f17e3f3df60e12bedbac647119a892
Sanket_Nimsatkar <121743873+nimsatkarS@users.noreply.github.com>
2023-05-10 17:50:09 +0530

    TimeOut Interval

---
+++ setinterval.js
@@ -0,0 +1,17 @@
+function greet(name,chainel){ 
+    console.log("This is the SetTimeout"+ name + ": " + chainel);
+    
+    }
+    setTimeout(greet,5000,"Ankita","Back to source")
+ 
+    timeout = setTimeout(greet,5000,"sanket is bad boy")
+    console,log(timeout);
+ 
+  //This is the DISPLAY TIME FUNCTION 
+    /*function displayTime(){
+     time = new Date();
+     console.log(time);
+  document.getElementById("time").innerHTML = time;
+ }
+ setInterval(displayTime, 1000);
+ */
